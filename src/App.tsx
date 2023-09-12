677a3101fc6e8dda0519bee0f5f9ac1a6dd49b0d
nauvaluchiha <142927902+nauvaluchiha@users.noreply.github.com>
2023-09-12 09:07:37 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,22 @@
-import { createRoutesFromElements, createBrowserRouter, Route, Link, Outlet, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CreateProduct } from "@/Pages/CreateProduct";
-// import Navbar from "@/components/Nav/NavBar";
+import Navbar from "@/components/Nav/NavBar";
 // Import our custom CSS
 // import './styles.scss'
 // Import all of Bootstrap's JS
 // import * as bootstrap from 'bootstrap'
 
 function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<Root />}>
-        <Route index element={<CreateProduct />} />
-        <Route path="/tes" element={<h1>Tes</h1>} />
-        <Route path="*" element={<h1>Not Foundddd</h1>} />
-      </Route>
-    )
-  )
-
   return (
-    <div>
-      <RouterProvider router={router} />
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<CreateProduct />} />
+        <Route path="/createproduct" element={<CreateProduct />} />
+        <Route path="*" element={<h1>Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
-}
-
-const Root = () => {
-  return (
-    <>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/createproduct">Create Product</Link>
-      </div>
-      <div>
-        <Outlet />
-      </div>
-    </>
-  )
 }
 
 export default App;
