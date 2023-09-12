31efd4cfd0d6da51e68eb4724d68ee803e4b86fd
nauvaluchiha <142927902+nauvaluchiha@users.noreply.github.com>
2023-09-12 09:00:47 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
     createRoutesFromElements(
       <Route path="/" element={<Root />}>
         <Route index element={<h1>Ini home</h1>} />
-        <Route path="/createproduct" element={<CreateProduct /></Route>} />
+        <Route path="/createproduct" element={<CreateProduct />} />
         <Route path="/tes" element={<h1>Tes</h1>} />
         <Route path="*" element={<h1>Not Foundddd</h1>} />
       </Route>
